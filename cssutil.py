31d2b48caee230abf4786e8f3eff49f52a882a75
Daniel Monaghan <mowglixx@users.noreply.github.com>
2022-04-14 20:41:26 +0100

    added project tracking

--- cssutil.py
+++ cssutil.py
@@ -2,29 +2,44 @@
 #   Written By Dan Monaghan 04/2022 onwards
 #
 
-project = {
-    'name': 'CssUtil',
-    'version': '0.2',
-    'author': 'Dan Monaghan',
-    'github': 'https://github.com/mowglixx/cssutil',
-}
+class Project: 
+    def __init__(self, name, version, author, github) -> None:
+        self.name = name
+        self.version = version
+        self.author = author
+        self.github = github
 
-# setup alphabet constant for filename checking 
-ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+project = Project(
+    'CssUtil', 
+    0.2, 
+    'Dan Monaghan', 
+    'https://github.com/mowglixx/cssutil'
+    )
+
+# setup alphabet constant for filename checking
+ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 ALPHABET_UPPER = []
 for letter in ALPHABET:
     ALPHABET_UPPER.append(letter.upper())
 ALPHABET_UPPER.extend(ALPHABET)
 
+
 # global css string for output and appending to with header
-css=f'''/*
+css = f'''/*
 ########################################
 
-{project['name']} version: {project['version']}
-By {project['author']} <{project['github']}>
+{project.name} version: {project.version}
+By {project.author} <{project.github}>
 
 ########################################
 */'''
+class_list = [
+    '# ', project.name, ' v', str(project.version), '\n',
+    '#### Author: ', project.author, ' - ',
+    '[Github](', project.github, ')\n',
+    '## Class List\n\nThis is the auto generated class list',
+    ]
 css += '''
 :root {
     /* Light and dark colors */
@@ -67,7 +82,7 @@ button:hover, a:hover{
 '''
 
 # hsl in css is a 360deg wheel of color
-# below is the 12 distinct colors paired with the degree on the color wheel 
+# below is the 12 distinct colors paired with the degree on the color wheel
 hue_colors = {
     0: 'red',
     30: 'orange',
@@ -99,12 +114,17 @@ class CSS_Selector:
 
     def __init__(self, class_name: str = 'class-name', properties: list[str] = ['property: value']) -> str:
         global css
+        global class_list
 
         self.class_name = class_name
         self.properties = properties
 
+        class_list.append(f'\n- `.{self.class_name}`\n')
+        for property in properties:
+            class_list.append(f'  - `{property}`\n')
+        class_list.append(f'\n')
         css += self.return_string()
-        print(f'.{self.class_name}', 'added to css!')
+        # print(f'.{self.class_name}', 'added to css!')
 
     def return_string(self):
         selector = f'\n.{self.class_name}'+"{\n"
@@ -120,8 +140,6 @@ class CSS_Selector:
         return self.class_name
 
 
-
-
 def output_css():
 
     def check_filename():
@@ -129,7 +147,7 @@ def output_css():
         global ALPHABET
         legal_symbols = ['.', ' ', '[', ']', '-', '/']
         filename = input('Please input a filename: [main.css]')
-        
+
         if filename == '':
             filename = 'main.css'
 
@@ -154,9 +172,11 @@ def output_css():
     f = open(filename, "w")  # open the file
     f_min = open(min_filename(), "w")  # open the file
     f.write(css)  # saving the file
-    f_min.write(css.strip().replace('\n', '').replace('    ', ''))  # saving the file
+    f_min.write(css.strip().replace('\n', '').replace(
+        '    ', ''))  # saving the file
     print(f'"./{filename}" and "./{min_filename()}"', 'written successfully!')
     f.close()  # close the file
+
 
 # initialisation of base classes
 CSS_Selector('flex', ['display: flex'])
@@ -166,7 +186,7 @@ CSS_Selector('flex-wrap', ['flex-wrap: wrap'])
 CSS_Selector('flex-nowrap', ['flex-wrap: nowrap'])
 CSS_Selector('flex-center', ['justify-content: center'])
 CSS_Selector('flex-between', ['justify-content: space-between'])
-CSS_Selector('text-center', ['flex-wrap: wrap'])
+CSS_Selector('text-center', ['text-align: center'])
 CSS_Selector('rounded', ['border-radius: 1em'])
 CSS_Selector('bg-dark', ['background-color: var(--dark-color)'])
 CSS_Selector('text-dark', ['color: var(--dark-color)'])
@@ -181,8 +201,6 @@ CSS_Selector('transition-color', ['transition: color 0.3s'])
 CSS_Selector('transition-filter', ['transition: filter 0.3s'])
 CSS_Selector('transition-background', ['transition: background 0.3s'])
 
-
-
 # uniform sizing (p-*, m-*)
 measurements = {
     'm': 'margin',
@@ -193,7 +211,8 @@ measurements = {
 for (short_bound, bound) in measurements.items():
     count = 0
     while count < 9:
-        CSS_Selector(f'{short_bound}-{count}', [f'{bound}: {round(count*measuring_step, 2)}em'])
+        CSS_Selector(f'{short_bound}-{count}',
+                     [f'{bound}: {round(count*measuring_step, 2)}em'])
         count += 1
 
 # individual sizing (.pl-*, .pr-*, .pt-*, pb-*, .ml-*, .mr-*, .mt-*, mb-*)
@@ -207,16 +226,17 @@ for (short_bound, bound) in measurements.items():
     count = 0
     while count < 9:
         for (shortdir, dir) in directions.items():
-            CSS_Selector(f'{short_bound}{shortdir}-{count}', [f'{bound}-{dir}: {round(count*measuring_step, 2)}em'])
+            CSS_Selector(f'{short_bound}{shortdir}-{count}',
+                         [f'{bound}-{dir}: {round(count*measuring_step, 2)}em'])
         count += 1
 
 # combined sizes (px-*, mx-*)
 directions = {
     'x': [
-        'left', 
-        'right'], 
+        'left',
+        'right'],
     'y': [
-        'top', 
+        'top',
         'bottom']}
 measurements = {
     'm': 'margin',
@@ -225,17 +245,20 @@ measurements = {
 
 for (axis, dir) in directions.items():
     for (short_bound, property) in measurements.items():
-        for mult in range(0,9):
+        for mult in range(0, 9):
             CSS_Selector(
-                f'{short_bound}{axis}-{mult}', 
+                f'{short_bound}{axis}-{mult}',
                 [
-                    f'{property}-{dir[0]}: {round(mult*measuring_step, 2)}em', 
+                    f'{property}-{dir[0]}: {round(mult*measuring_step, 2)}em',
                     f'{property}-{dir[1]}: {round(mult*measuring_step, 2)}em'
                 ])
 
+class_list.append('\n\n## Colours / colors\n')
 # color generation
 for (hue_color, color) in hue_colors.items():
+    class_list.append(f'\n### {color.title()}\n')
     css = css + f'\n/*[ {color} ]*/'
+    
     shades = range(1000, 0, -100)
     for shade in shades:
         # if the shade is not 0 or 1000
@@ -260,13 +283,13 @@ for (hue_color, color) in hue_colors.items():
                 f'text-{color}-{shade}',
                 [
                     f'color: hsl(var(--{color}-color), var(--color-saturation), {shade//10}%)'
-                    ])
+                ])
             for pseudo in ['hover', 'active', 'visited']:
                 CSS_Selector(
                     f'text-{color}-{shade}-{pseudo}:{pseudo}',
                     [
                         f'color: hsl(var(--{color}-color), var(--color-saturation), {shade//10}%)'
-                        ])
+                    ])
 
             # borders
             CSS_Selector(
@@ -282,4 +305,10 @@ for (hue_color, color) in hue_colors.items():
                     ])
 
 print('Everything Seemed to go well...')
+# writes the class_list.md file
+f = open('class_list.md', "w")  # open the file
+class_list
+f.write(''.join(class_list))  # saving the file
+f.close()  # close the file
+
 output_css()
